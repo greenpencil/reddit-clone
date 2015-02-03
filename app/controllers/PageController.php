d3fd06c36ae06e6bb40e2637c8cae59897dc42bd
Katie Paxton-Fear <K.Paxton-Fear@edu.salford.ac.uk>
2015-02-03 20:16:38 +0000

    Many to many issues

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -4,6 +4,18 @@ class PageController extends BaseController{
 
     function frontpage()
     {
+        $subscriptions = User::whereUsername(Auth::user()->username)->first()->subscriptions;
+        $posts = null;
+        foreach($subscriptions as $subscription)
+        {
+            $posts = Post::whereSubreddit($subscription->title);
+        }
+        return View::make('pages.frontpage', ['posts' => $posts]);
+    }
+
+
+    function all()
+    {
         $posts = Post::all();
         return View::make('pages.frontpage', ['posts' => $posts]);
     }
